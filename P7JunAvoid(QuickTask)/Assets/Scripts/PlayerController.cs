efc24adf35bd7734f739b7ab3eaa0e40bfe236e2
DozingDozerZZZZ <763664@student.dusd.net>
2026-08-19 13:49:22 -0700

    getting started

---
+++ P7JunAvoid(QuickTask)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
